9013bf17000d081a1377f8153da332b811ac515f
Gurmeet <you@example.com>
2025-04-04 16:14:15 +0530

    fifth commit

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono';
-import { handle } from 'hono/vercel';
+import { handle } from 'hono/netlify';
 
 import auth from "@/features/auth/server/route";
 import members from "@/features/members/server/route"; 
